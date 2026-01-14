647066dfe454ba7abe638cdf13cf83b9f58a0835
malmstli <malmstli@arcada.fi>
2026-01-14 15:31:15 +0200

    initial commit

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    private String name; // this.name
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("hello");
+
+        Car car = new Car( "VW" );
+        Car car2 = new Car( "Tesla");
+
+        System.out.println(car.getName());
+        car.setName("volvo");
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
     }
 }
