2f30a9bed14a98b3e3df551461ca1e365ea26871
Linus Malmström <malmstli@arcada.fi>
2026-01-23 11:12:28 +0200

    cc random rundown

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner myObj = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("enter för nytt, q för att avsluta");
+            String val = myObj.nextLine();
+
+            if (val.equalsIgnoreCase("q")) {
+                System.out.println("avslutas");
+                break;
+            }
+
+            if (val.isEmpty()) {
+                int tal = rand.nextInt(20);
+                System.out.println(tal);
+            }
+        }
     }
 }
